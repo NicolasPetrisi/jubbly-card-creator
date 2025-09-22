f37fafe51ae6d8e5738e913b6a0a34a894666ac8
NicolasPetrisi <nicolas97p@hotmail.com>
2025-09-22 22:29:54 +0200

    Added div location for unique card type. Fixed blue color to match the one used in game. Started making the cards from the game and made slight adjustments to better match that from the game.

--- projects/wow_class_cards/utils.js
+++ projects/wow_class_cards/utils.js
@@ -2,8 +2,8 @@ const widthBot = "12";
 const height = "8"
 
 // Calculates the ratios of the trapezoid for dice icon.
-const widthTopLeft = widthBot / 3;
-const widthTopRight = (widthBot * 2) / 3;
+const widthTopLeft = widthBot / 2.5;
+const widthTopRight = (widthBot * 2) / 3.4;
 const points = `${widthTopLeft} 0, ${widthTopRight} 0, ${widthBot} ${height}, 0 ${height}`;
 const viewBox = `0 0 ${widthBot} ${height}`;
 
@@ -15,9 +15,13 @@ class DiceOneColor extends HTMLElement {
     constructor() {
         super();
 
-        const color = this.getAttribute('c') || 'white';
+        let color = this.getAttribute('c') || 'white';
         const number = this.getAttribute('n') || '';
-        const plus = this.getAttribute('p') || 'n';
+        const plus = this.hasAttribute('p');
+
+        if (color === 'blue') {
+            color = '#1096d4'
+        }
 
         this.innerHTML = `
       <div class="trapezoid-wrapper">
@@ -36,7 +40,7 @@ class DiceOneColor extends HTMLElement {
             dominant-baseline="middle" 
             class="trapezoid-num">${number}</text>
         </svg>
-        ${plus === 'y' ? `<div class="trapezoid-plus">+</div>` : ''}
+        ${plus ? `<div class="trapezoid-plus">+</div>` : ''}
       </div>`;
     }
 }
@@ -45,16 +49,25 @@ class DiceTwoColors extends HTMLElement {
     constructor() {
         super();
 
-        const color_1 = this.getAttribute('c1') || 'white';
-        const color_2 = this.getAttribute('c2') || 'white';
+        let color_1 = this.getAttribute('c1') || 'white';
+        let color_2 = this.getAttribute('c2') || 'white';
         const number = this.getAttribute('n') || '';
-        const plus = this.getAttribute('p') || 'n';
+        const plus = this.hasAttribute('p');
+
+        if (color_1 === 'blue') {
+            color_1 = '#1096d4'
+        }
+        if (color_2 === 'blue') {
+            color_2 = '#1096d4'
+        }
+
+        const gradientId = `twoColorGradient-${plus}-${color_1}-${color_2}`;
 
         this.innerHTML = `
-      <div class="trapezoid-wrapper ${plus === 'y' ? 'has-plus' : ''}">
+      <div class="trapezoid-wrapper ${plus ? 'has-plus' : ''}">
         <svg class="trapezoid" width=${widthBot} height=${height} viewBox="${viewBox}">
           <defs>
-            <linearGradient id="twoColorGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <linearGradient id="${gradientId}" x1="0%" y1="0%" x2="100%" y2="0%">
               <stop offset="0%" stop-color="${color_1}" />
               <stop offset="48%" stop-color="${color_1}" />
               <stop offset="49%" stop-color="black" />
@@ -66,7 +79,7 @@ class DiceTwoColors extends HTMLElement {
           <!-- trapezoid shape with border -->
           <polygon 
             points="${points}"
-            fill="url(#twoColorGradient)" 
+            fill="url(#${gradientId})" 
             stroke="black" 
             stroke-width="0.5"/>
           <!-- centered number -->
@@ -77,6 +90,7 @@ class DiceTwoColors extends HTMLElement {
             dominant-baseline="middle" 
             class="trapezoid-num">${number}</text>
         </svg>
+        ${plus ? `<div class="trapezoid-plus">+</div>` : ''}
       </div>
     `;
     }
